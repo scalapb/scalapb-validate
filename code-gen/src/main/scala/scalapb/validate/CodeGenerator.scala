e3003d0385cccaa592b19029b36087511c5f5d3f
Arsene Tochemey GANDOTE <tochemey@hotmail.com>
2020-06-06 01:00:27 +0000

    complete String validation generation code

--- code-gen/src/main/scala/scalapb/validate/CodeGenerator.scala
+++ code-gen/src/main/scala/scalapb/validate/CodeGenerator.scala
@@ -1,24 +1,25 @@
 package scalapb.validate
 
+import com.google.protobuf.Descriptors.{Descriptor, FieldDescriptor}
 import com.google.protobuf.ExtensionRegistry
-import scalapb.options.compiler.Scalapb
-import scalapb.compiler.ProtobufGenerator
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse
-import protocbridge.Artifact
-import scalapb.validate.compat.JavaConverters._
-import scalapb.compiler.DescriptorImplicits
-import com.google.protobuf.Descriptors.Descriptor
-import scalapb.compiler.FunctionalPrinter
-import protocbridge.codegen.CodeGenApp
-import protocbridge.codegen.CodeGenResponse
-import protocbridge.codegen.CodeGenRequest
-import com.google.protobuf.Descriptors.FieldDescriptor
 import io.envoyproxy.pgv.validate.Validate
 import io.envoyproxy.pgv.validate.Validate.FieldRules.TypeCase
-import io.envoyproxy.pgv.validate.Validate.StringRules
-import io.envoyproxy.pgv.validate.Validate.MessageRules
-import io.envoyproxy.pgv.validate.Validate.UInt64Rules
-import io.envoyproxy.pgv.validate.Validate.FieldRules
+import io.envoyproxy.pgv.validate.Validate.{
+  FieldRules,
+  MessageRules,
+  StringRules,
+  UInt64Rules
+}
+import protocbridge.Artifact
+import protocbridge.codegen.{CodeGenApp, CodeGenRequest, CodeGenResponse}
+import scalapb.compiler.{
+  DescriptorImplicits,
+  FunctionalPrinter,
+  ProtobufGenerator
+}
+import scalapb.options.compiler.Scalapb
+import scalapb.validate.compat.JavaConverters._
 
 object CodeGenerator extends CodeGenApp {
   override def registerExtensions(registry: ExtensionRegistry): Unit = {
@@ -58,8 +59,8 @@ class MessagePrinter(
     implicits: DescriptorImplicits,
     message: Descriptor
 ) {
-  import implicits._
   import DescriptorImplicits.AsSymbolPimp
+  import implicits._
 
   private def validatorName(fd: Descriptor): ScalaName =
     if (fd.isTopLevel)
@@ -139,7 +140,132 @@ class MessagePrinter(
       )
     else None
 
-    maybeLen ++ maybeLenBytes ++ maybeEmail ++ maybePattern
+    val maybePrefix =
+      if (rules.hasPrefix())
+        Option(s"""$SV.prefix("$name", $inputExpr, ${rules.getPrefix})""")
+      else None
+
+    val maybeSuffix =
+      if (rules.hasSuffix())
+        Option(s"""$SV.suffix("$name", $inputExpr, ${rules.getSuffix})""")
+      else None
+
+    val maybeContains =
+      if (rules.hasContains)
+        Option(s"""$SV.contains("$name", $inputExpr, ${rules.getContains})""")
+      else None
+
+    val maybeNotContains =
+      if (rules.hasNotContains)
+        Option(
+          s"""$SV.notContains("$name", $inputExpr, ${rules.getNotContains})"""
+        )
+      else None
+
+    val maybeHostName =
+      if (rules.getHostname)
+        Option(
+          s"""$SV.hostName("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeIp =
+      if (rules.getIp)
+        Option(
+          s"""$SV.ip("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeIpv4 =
+      if (rules.getIpv4)
+        Option(
+          s"""$SV.ipv4("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeIpv6 =
+      if (rules.getIpv6)
+        Option(
+          s"""$SV.ipv6("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeUri =
+      if (rules.getUri)
+        Option(
+          s"""$SV.uri("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeUriRef =
+      if (rules.getUriRef)
+        Option(
+          s"""$SV.uriRef("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeUuid =
+      if (rules.getUuid)
+        Option(
+          s"""$SV.uuid("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeAddress =
+      if (rules.getAddress)
+        Option(
+          s"""$SV.address("$name", $inputExpr)"""
+        )
+      else None
+
+    val maybeMaxBytes =
+      if (rules.hasMaxBytes)
+        Option(
+          s"""$SV.maxBytes("$name", $inputExpr, ${rules.getMaxBytes})"""
+        )
+      else None
+
+    val maybeMinBytes =
+      if (rules.hasMinBytes)
+        Option(
+          s"""$SV.minBytes("$name", $inputExpr, ${rules.getMinBytes})"""
+        )
+      else None
+
+    val maybeMiniLength =
+      if (rules.hasMinLen)
+        Option(
+          s"""$SV.minLength("$name", $inputExpr, ${rules.getMinLen})"""
+        )
+      else None
+
+    val maybeMaxLength =
+      if (rules.hasMaxLen)
+        Option(
+          s"""$SV.maxLength("$name", $inputExpr, ${rules.getMaxLen})"""
+        )
+      else None
+
+    maybeAddress
+      .concat(maybeContains)
+      .concat(maybeEmail)
+      .concat(maybeHostName)
+      .concat(maybeIp)
+      .concat(maybeIpv4)
+      .concat(maybeIpv6)
+      .concat(maybeLen)
+      .concat(maybeLenBytes)
+      .concat(maybeMaxBytes)
+      .concat(maybeMaxLength)
+      .concat(maybeMinBytes)
+      .concat(maybeMiniLength)
+      .concat(maybeNotContains)
+      .concat(maybePattern)
+      .concat(maybePrefix)
+      .concat(maybeSuffix)
+      .concat(maybeUuid)
+      .concat(maybeUriRef)
+      .concat(maybeUri)
   }
 
   def uint64Rules(
@@ -147,13 +273,12 @@ class MessagePrinter(
       inputExpr: String,
       rules: UInt64Rules
   ): Iterable[String] = {
-    val name = fieldDescriptor.getName()
+    val name = fieldDescriptor.getName
     val CV = "io.envoyproxy.pgv.ComparativeValidation"
     val maybeGt =
-      if (rules.hasGt())
+      if (rules.hasGt)
         Option(
-          s"""$CV.greaterThan[java.lang.Long]("$name", $inputExpr, ${rules
-            .getGt()}, java.util.Comparator.naturalOrder)"""
+          s"""$CV.greaterThan[java.lang.Long]("$name", $inputExpr, ${rules.getGt}, java.util.Comparator.naturalOrder)"""
         )
       else None
 
@@ -165,16 +290,16 @@ class MessagePrinter(
       inputExpr: String,
       rules: MessageRules
   ): Iterable[String] = {
-    val name = fieldDescriptor.getName()
+    val name = fieldDescriptor.getName
 
     val maybeRequired =
-      if (rules.getRequired())
+      if (rules.getRequired)
         Some(
           s"""if ($inputExpr.isEmpty) throw new io.envoyproxy.pgv.ValidationException("$name", "None", "is required")"""
         )
       else None
 
-    val inner = if (!rules.getSkip()) {
+    val inner = if (!rules.getSkip) {
       val msgType = fieldDescriptor.getMessageType
       val cmpName = validatorName(msgType).fullName
       // doesn't work correctly yet - since validate doesn't throw an exception
@@ -191,12 +316,12 @@ class MessagePrinter(
       input: String,
       rules: FieldRules
   ): Iterable[String] =
-    rules.getTypeCase() match {
-      case TypeCase.STRING => stringRules(fd, input, rules.getString())
-      case TypeCase.UINT64 => uint64Rules(fd, input, rules.getUint64())
+    rules.getTypeCase match {
+      case TypeCase.STRING => stringRules(fd, input, rules.getString)
+      case TypeCase.UINT64 => uint64Rules(fd, input, rules.getUint64)
       case TypeCase.TYPE_NOT_SET =>
-        if (fd.isMessage && rules.hasMessage())
-          messageRules(fd, input, rules.getMessage())
+        if (fd.isMessage && rules.hasMessage)
+          messageRules(fd, input, rules.getMessage)
         else Seq.empty
       case _ => Seq.empty
     }
@@ -205,7 +330,7 @@ class MessagePrinter(
       fp: FunctionalPrinter,
       fd: FieldDescriptor
   ): FunctionalPrinter = {
-    val rules = fd.getOptions().getExtension(Validate.rules)
+    val rules = fd.getOptions.getExtension(Validate.rules)
     val accessor = s"input.${fd.scalaName.asSymbol}"
     if (fd.supportsPresence && !fd.isMessage) {
       val validations = rulesSingle(fd, "_value", rules).toSeq
