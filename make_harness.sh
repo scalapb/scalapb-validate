de1dbb1ade2dc2ad3366678763b66019687d159e
Nadav Samet <thesamet@gmail.com>
2020-11-18 08:29:17 -0800

    Revert "Update to pgv 0.4.1"
    This reverts commit f37ba13ed14224ab9e6a499b6176f00e6a520e66.

--- make_harness.sh
+++ make_harness.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash 
 set -e
 ./get_bazelisk.sh
-git clone https://github.com/envoyproxy/protoc-gen-validate.git .pgv || git -C .pgv pull
+git clone https://github.com/thesamet/protoc-gen-validate.git .pgv || git -C .pgv pull
 cd .pgv
-git checkout v0.4.1
+git checkout v0.3.0
 ../bazelisk build tests/harness/executor --build_event_json_file=out.json
 binfile=$(jq -r '.completed.importantOutput[0].name|select(.!=null)' out.json)
 cp bazel-bin/$binfile ./executor.exe
