08c18b42ee1220306b9fd8633c50ef7009aa1ae4
Nadav Samet <thesamet@gmail.com>
2022-01-16 09:17:12 -0800

    Sync pgv tests to 0.6.3

--- sync_pgv_tests.sh
+++ sync_pgv_tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-TAG=0.6.1-java
+TAG=0.6.3-java
 
 curl -L https://github.com/envoyproxy/protoc-gen-validate/archive/v$TAG.tar.gz | \
   tar xvz --strip-components=1 -C e2e/src/main/protobuf protoc-gen-validate-$TAG/tests
